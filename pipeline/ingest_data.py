6fa16bd053949f455bd59b9cb9d333bee08ff4ac
makarellav <153069939+makarellav@users.noreply.github.com>
2026-04-12 20:05:53 +0000

    progress

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,20 +4,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
+import click
 
 year = 2021
 month = 1
-
-pg_user = 'root'
-pg_pass = 'root'
-pg_host = 'localhost'
-pg_db = 'ny_taxi'
-pg_port = 5432
-
-chunksize = 100000
-
-target_table = 'ny_taxi'
 
 prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
 url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
@@ -46,7 +36,15 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='ny_taxi', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Ingestion chunk size')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize):
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
 
     df_iter = pd.read_csv(
